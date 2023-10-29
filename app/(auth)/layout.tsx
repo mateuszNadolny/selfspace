c77b31aa119cdbf6e670a5490ee5981b527f04c0
mateuszNadolny <mateusznadolny00@gmail.com>
2023-10-29 22:13:46 +0100

    prepared files for deployment

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
     return (
         <div className="flex items-center justify-center h-screen w-full bg-[#253334]">
