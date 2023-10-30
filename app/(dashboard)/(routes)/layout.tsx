7f0f1e83eb836af77bde671378985b64af9fca9f
mateuszNadolny <mateusznadolny00@gmail.com>
2023-10-30 21:23:22 +0100

    added 'back' button on navigation

--- app/(dashboard)/(routes)/layout.tsx
+++ app/(dashboard)/(routes)/layout.tsx
@@ -1,8 +1,28 @@
+'use client'
+
 import { UserButton } from '@clerk/nextjs'
 
+import Link from 'next/link'
+import Image from 'next/image'
+import { usePathname } from 'next/navigation'
+
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+    const pathname = usePathname()
     return (
         <div className="max-h-screen h-screen w-full relative">
+            <div className="absolute z-10 top-[15px] left-[10px] text-slate-400 text-sm font-sans">
+                {pathname !== '/dashboard' && (
+                    <div className="flex items-center justify-center gap-1">
+                        <Image
+                            src={'/arrow-left.svg'}
+                            alt="arrow"
+                            width={9}
+                            height={9}
+                        />
+                        <Link href="/dashboard">Dashboard</Link>
+                    </div>
+                )}
+            </div>
             <div className="absolute z-10 top-[10px] right-[10px]">
                 <UserButton />
             </div>
