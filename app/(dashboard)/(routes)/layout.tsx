52b7d193efeb4c8c8380e268d88ece3d7afab498
mateuszNadolny <mateusznadolny00@gmail.com>
2023-10-30 11:56:55 +0100

    applied the redesign of layou.tsx

--- app/(dashboard)/(routes)/layout.tsx
+++ app/(dashboard)/(routes)/layout.tsx
@@ -1,10 +1,12 @@
-import Footer from '@/components/custom/Footer'
+import { UserButton } from '@clerk/nextjs'
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
     return (
         <div className="max-h-screen h-screen w-full relative">
+            <div className="absolute z-10 top-[10px] right-[10px]">
+                <UserButton />
+            </div>
             {children}
-            <Footer />
         </div>
     )
 }
