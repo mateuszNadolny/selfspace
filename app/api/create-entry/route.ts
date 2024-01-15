0ead4688d65d3593246ad2ba15dbb95c98ced64b
mateuszNadolny <mateusznadolny00@gmail.com>
2024-01-15 23:08:07 +0100

    fix: updated the route handlers by returning new Response

--- app/api/create-entry/route.ts
+++ app/api/create-entry/route.ts
@@ -1,9 +1,8 @@
-import { NextResponse } from 'next/server'
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-export async function POST(request: Request): Promise<Response> {
+export async function POST(request: Request) {
     try {
         const body = await request.json()
         const { title, entry, userId } = body
