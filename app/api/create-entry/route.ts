be72a663edb0bac248e52ecbf7294d6450a11763
mateuszNadolny <mateusznadolny00@gmail.com>
2024-01-15 22:47:46 +0100

    test

--- app/api/create-entry/route.ts
+++ app/api/create-entry/route.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-export async function POST(request: Request) {
+export default async function POST(request: Request) {
     try {
         const body = await request.json()
         const { title, entry, userId } = body
