71da493476d1e929527961c31649c43c726e1377
mateuszNadolny <mateusznadolny00@gmail.com>
2024-01-15 22:15:42 +0100

    test fix

--- app/api/create-entry/route.ts
+++ app/api/create-entry/route.ts
@@ -15,10 +15,17 @@ export async function POST(request: Request) {
             },
         })
 
-        return Response.json(post, { status: 201 })
+        const json = JSON.stringify(post)
+
+        return new Response(json, {
+            status: 201,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
     } catch (error) {
         console.error('Error in POST /api/create-entry:', error)
         console.log(error)
-        return Response.error
+        return Response.error()
     }
 }
